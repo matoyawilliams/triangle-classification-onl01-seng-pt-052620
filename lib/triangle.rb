86970e336f443c527fa5c2e0e9f5b8d1156c84c4
Matoya Williams <matoyawilliams@yahoo.com>
2020-07-01 21:02:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,70 +1,39 @@
 class Triangle
-  def initialize(side_1,side_2,side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
-  end
-
-  def kind()
-    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      raise TriangleError
-    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-      raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
-      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
-      end
-    end
-
-  end
-
-end
-
-class TriangleError < StandardError
-  # triangle error code
-endclass Triangle
-  # write code here\
-
-  def initialize(s1, s2, s3)
-
-  
-    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
-      raise TriangleError
-    end
-
-    if s1 <= 0 || s2 <= 0 ||s3 <= 0
-      raise TriangleError
-    end
-
-    if s1 == nil || s2 == nil ||s3 == nil
-      raise TriangleError
-    end
-
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end
 
   def kind
-    if @s1 == @s2 && @s1 == @s3
-      return :equilateral
-    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
-      return :isosceles
-    else
-      return :scalene
-    end
+    if kosher_triangle == true && kosher_triangle2 == true 
+      puts true 
+    else 
+      raise TriangleError 
+    end 
+    
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
   end
-
+  
+  def kosher_triangle 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def kosher_triangle2 
+    x + y > z && x + z > y && y + z > x
+  end 
 end
-
-
 
 class TriangleError < StandardError
-  def message
-    "This is not a valid triangle."
-  end
+  
 end
+
+a = Triangle.new(5, 5, 5)
+a.kind
